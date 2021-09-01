e8d3dd7b090eacabd217b411d07d7bc71c5604d8
yagocenturion10 <84146773+yagocenturion10@users.noreply.github.com>
2021-09-01 02:27:08 -0300

    Segunda entrega de curso de RJS
    Segunda entrega de curso de RJS

---
+++ src/components/NavBar.js
@@ -0,0 +1,45 @@
+
+
+
+const NavBar = () => {
+  
+    return (
+    
+     
+    <header>
+      <nav className="navbar fixed-top navbar-expand-lg navbar-dark scrolling-navbar">
+        <a className="navbar-brand" href="index.html"><img src="gianlogo5.png" alt="Volver al Inicio" width={250} height={75} /> </a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="index.html">Inicio <span className="sr-only">(current)</span></a>   
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="productos.html">Productos</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="contacto.html">Contacto</a>
+            </li>
+          </ul>
+          <ul className="navbar-nav nav-flex-icons">
+            <li className="nav-item">
+              <a href="https://www.facebook.com/genuina.beer" target="_blank" className="nav-link"><i className="fab fa-facebook-f" /></a>
+            </li>
+            <li className="nav-item">
+              <a a href="https://www.twitter.com/genuina.beer" target="_blank" className="nav-link"><i className="fab fa-twitter" /></a>
+            </li>
+            <li className="nav-item">
+              <a href="https://www.instagram.com/genuina.beer" target="_blank" className="nav-link"><i className="fab fa-instagram" /></a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+    
+    )
+    }
+
+export default NavBar
