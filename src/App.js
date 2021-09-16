85ba97bf303e9029e798aea2b377d14adfb8ef53
yagocenturion10 <yago.centurion10@gmail.com>
2021-09-16 00:19:26 -0300

    promesas

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {NavBar} from "./NavBar"
@@ -5,28 +6,77 @@ import { ItemListContainer } from './ItemListContainer';
 
 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <NavBar/>
-        <ItemListContainer saludo="¡Bienvenido!"/>
-        
-      </header>
-    </div>
-  );
+
+const productos = [
+
+  {
+    precio: 5000,
+   id: '1',
+    title: 'Barril 10L',
+
+  },
+  {
+    precio: 7000,
+   id: '2',
+    title: 'Barril 30L',
+
+  },
+  {
+    precio: 15000,
+    id: '3',
+     title: 'Barril 50L',
+
+  }
+]
+
+function getList() {
+return new Promise ((resolve, reject) => {
+  setTimeout(() => resolve(productos), 15000)
+})
 }
+
+const App = () => {
+
+const [listBirra, setListBirra] = useState ([]) 
+
+useEffect (() => {
+
+const list = getList()
+
+  list.then(list => {
+    setListBirra(list)
+
+  })
+
+  
+}, [])
+
+
+
+
+return (
+  <div className="App">
+    <header>
+
+    
+
+      <ItemListContainer saludo="¡Bienvenido!"/>
+
+    </header>
+ <div>
+   <ul>
+   {productos.map(beer => <li key={beer.id}>{beer.title}</li>)}
+   </ul>
+ </div>
+
+  <itemCount/>
+  
+  <img scr={logo} className="App-logo" alt="logo" />
+
+  </div>
+);
+}
+
+
 
 export default App;
